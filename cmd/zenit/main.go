7afe50a1be787695fe086cb9443af59add7afa0c
James Dennes <jdennes@gmail.com>
2016-02-23 11:17:28 +0100

    Add go-octokit and begin validating X-Hub-Signature header

--- cmd/zenit/main.go
+++ cmd/zenit/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"crypto/sha256"
+	"crypto/hmac"
 
 	"github.com/gin-gonic/gin"
+	"github.com/octokit/go-octokit/octokit"
 )
 
-func handlePush(c *gin.Context) {
-	c.String(http.StatusOK, "Handling a push event")
+func checkSecret(context *gin.Context) bool {
+	// X-Hub-Signature header format:
+	// https://github.com/github/github-services/blob/8dc2328d0d97005e6431c7ca8c7de9466e38567e/lib/service/http_helper.rb#L76-L77
+	header := context.Request.Header.Get("X-Hub-Signature")
+	mac := hmac.New(sha256.New, []byte(os.Getenv("SECRET")))
+	body := ""
+	mac.Write([]byte(body))
+	expectedMAC := mac.Sum(nil)
+	if !hmac.Equal([]byte(header), []byte(fmt.Sprintf("sha1=%v", expectedMAC))) {
+		context.String(http.StatusForbidden, "Unacceptable X-Hub-Signature HTTP header")
+		return false
+	}
+
+	return true
 }
 
-func handlePullRequest(c *gin.Context) {
-	c.String(http.StatusOK, "Handling a pull_request event")
+func handlePush(context *gin.Context, client *octokit.Client) {
+	if !checkSecret(context) {
+		return
+	}
+	context.String(http.StatusOK, "Handling a push event")
+}
+
+func handlePullRequest(context *gin.Context, client *octokit.Client) {
+	if !checkSecret(context) {
+		return
+	}
+	context.String(http.StatusOK, "Handling a pull_request event")
 }
 
 func main() {
@@ -22,21 +48,23 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
+	client := octokit.NewClient(octokit.TokenAuth{AccessToken: os.Getenv("TOKEN")})
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	router.GET("/", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	router.POST("/handle", func(c *gin.Context) {
-		event := c.Request.Header["X-Github-Event"][0]
+	router.POST("/handle", func(context *gin.Context) {
+		event := context.Request.Header.Get("X-Github-Event")
 		switch event {
 		case "push":
-			handlePush(c)
+			handlePush(context, client)
 		case "pull_request":
-			handlePullRequest(c)
+			handlePullRequest(context, client)
 		default:
 			log.Fatal("Unsupported event in the X-Github-Event HTTP header")
 		}
